12eb9671234e8ddcea2518370dbfb9bb1eb1cf95
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-04-21 13:45:40 -0500

    Update minirollfunction.py

--- py/minirollfunction.py
+++ py/minirollfunction.py
@@ -1,9 +1,9 @@
 import random
-def roll(dice):
+def cmd(dice):
   b=[]
   b=[]
   for i in dice.split('d'): 
     a.append(i)
   for i in range(int(a[0])):
     b.append(random.randint(1,int(a[1])))
-    return b
+  return b
